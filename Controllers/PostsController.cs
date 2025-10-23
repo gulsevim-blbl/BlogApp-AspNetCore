4de99c2fd74fc3ad63002f9f3701b0d476f369b7
gulsevim-blbl <gulsevimblbl@gmail.com>
2025-10-24 02:07:53 +0300

    Post Details

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -1,7 +1,9 @@
+using System.IO.Compression;
 using BlogApp_AspNetCore.Data.Abstract;
 using BlogApp_AspNetCore.Data.Concreate.EfCore;
 using BlogApp_AspNetCore.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogApp_AspNetCore.Controllers
 {
@@ -21,9 +23,14 @@ namespace BlogApp_AspNetCore.Controllers
                 new PostViewModel
                 {
                     Posts = _postRepository.Posts.ToList(),
-                   
+
                 }
             ); //veritabanindaki postlari listeleyip view e gonderiyorum
+        }
+        //Detay Sayfası oluşturalım
+        public  async Task<IActionResult> Details(int? id)
+        {
+            return View(await _postRepository.Posts.FirstOrDefaultAsync(p => p.PostId == id)); //dışarıdan id alıp o id ye sahip postu getiriyoruz
         }
     }
 }
